d7ae378dd1f6f46ffda569a8aefe3e104105940f
michael-chung-mc <chung.michael.mc@gmail.com>
2022-08-13 19:34:59 -0400

    Add adaptive size to grid cell

--- javascript.js
+++ javascript.js
@@ -1,4 +1,18 @@
+const button = document.querySelector('button');
 const grid = document.querySelector('.grid');
+
+var cellHeight = 8;
+var cellWidth = 8;
+
+function synchGridRowDimensions () {
+    const grid = document.getElementsByClassName('grid')[0];
+    const rows = document.getElementsByClassName('row');
+    const rowWidth = grid.offsetWidth;
+    console.log(`${rowWidth} width`);
+    cellHeight = rowWidth/rows.length;
+    cellWidth = rowWidth/rows.length;
+    console.log(`${cellHeight} height`);
+}
 
 function setGrid (rows, columns) {
     for (let i = 0; i < rows; i++)
@@ -14,11 +28,34 @@ function setGrid (rows, columns) {
             row.append(box);
         }
     }
+    resizeGrid();
 }
 
 function shadeCell (event) {
     let cell = event.target;
     cell.classList.add('shaded');
+}
+
+function clearGrid () {
+    while(grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+}
+
+function resizeGrid () {
+    synchGridRowDimensions();
+    let rows = grid.children;
+    for (let i = 0; i < rows.length; i++)
+    {
+        let row = rows[i];
+        row.style.height = `${cellHeight}px`;
+        console.log(`${row.style.height}`);
+        let cells = row.children;
+        for (let j = 0; j < cells.length; j++)
+        {
+            cells[j].style.width = `${cellWidth}px`;
+        }
+    }
 }
 
 setGrid(16,16);
